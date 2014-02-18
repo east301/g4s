c2e848af14ab3bbc2d1b058417e47efbdbcc0c03
east301 <me@east301.net>
2014-02-18 16:01:33 +0900

    show docstring of __init__ methods

--- build-api-doc.sh
+++ build-api-doc.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
-#
+# configuration
 MY_DIR=$(cd $(dirname $0); pwd)
 SRC_DIR=$MY_DIR/src
 API_DOC_DIR=$MY_DIR/api-doc
 
-#
+# runs `sphinx-apidoc`
 export PYTHONPATH=$SRC_DIR:$PYTHONPATH
 
 cd $MY_DIR
 sphinx-apidoc -F -f -o $API_DOC_DIR $SRC_DIR/g4s
 
+# modifies configuration
 cd $API_DOC_DIR
+
+cat >> conf.py <<EOF
+# -- autodoc configuration ------------------------------------------------
+autoclass_content = 'both'  # shows docstring of __init__
+EOF
+
+# builds api documents
 if [ $# -eq 0 ]; then
     make html
 else
